82087514af7fab59de5de29fa934d0b4972b3b59
nNlson rRmos Lopez <jerunel@hotmail.com>
2021-10-21 21:42:41 -0300

    terminado

--- bd/consultas.js
+++ bd/consultas.js
@@ -2,23 +2,105 @@ const { Pool } = require("pg");
 //const { pool } = require("pool");
 
 const pool = new Pool({
-user: "postgres",
-host: "localhost",
-password: "1234",
-database: "elecciones",
-port: 5432
+    user: "postgres",
+    host: "localhost",
+    password: "1234",
+    database: "elecciones",
+    port: 5432
 })
 
 const guardarCandidato = async (candidato) => {
-const values = Object.values(candidato);
-const consulta = {
-text: "INSERT INTO candidatos (nombre, foto, color, votos) values ($1,$2,$3,0)",
-values
-};
+    const values = Object.values(candidato);
+    const consulta = {
+        text: "INSERT INTO candidatos (nombre, foto, color, votos) values ($1,$2,$3,0)",
+        values
+    };
+    const result = await pool.query(consulta);
+    return result;
+}
 
-const result =  await pool.query(consulta);
-return result;
+
+const getCandidatos = async () => {
+    const result = await pool.query("SELECT * FROM candidatos");
+    return result.rows;
+}
+
+
+
+
+const editarCandidato = async (candidato) => {
+    const values = Object.values(candidato);
+    const consulta = {
+        text: "UPDATE candidatos SET nombre=$1, foto=$2 WHERE id=$3 RETURNING *",
+        values
+    };
+    const result = await pool.query(consulta);
+    return result;
+}
+
+
+
+
+const eliminarCandidato = async (id) => {
+    const result = await pool.query(`DELETE FROM candidatos WHERE id = ${id}`);
+    return result.rows;
+}
+
+
+
+
+
+const registroVotos = async (voto) => {
+    const values = Object.values(voto);
+
+
+    const registrarBotoHistorial = {
+        text: "INSERT INTO historial (estado, votos, ganador) values ($1,$2,$3)",
+        values
+    };
+
+
+
+    const registrarVotoCandidato = {
+        text: "UPDATE candidatos SET votos=votos+$1 WHERE nombre=$2",
+        values: [Number(values[1]), values[2]]
+    };
+
+
+    try {
+        await pool.query("BEGIN");
+        await pool.query(registrarBotoHistorial);
+        await pool.query(registrarVotoCandidato);
+        await pool.query("COMMIT");
+        return true;
+    } catch (e) {
+        console.log("error base de datos",e)
+        await Pool.query("ROLLBACK");
+        throw e;
+    }
+
+
+
 
 }
 
-module.exports = { guardarCandidato }
+
+
+
+const getHistorial = async () => {
+    const consulta = {
+text : "SELECT * FROM historial",
+rowMode: "array"
+    };
+
+    const result = await pool.query(consulta);
+    return result.rows;
+}
+
+
+
+
+
+
+
+module.exports = { guardarCandidato, getCandidatos, editarCandidato, eliminarCandidato,registroVotos,getHistorial }
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const http = require("http");
 const fs = require("fs");
-const { guardarCandidato } = require("./bd/consultas");
+const url = require("url");
+const { guardarCandidato, getCandidatos, editarCandidato, eliminarCandidato,registroVotos, getHistorial } = require("./bd/consultas");
 
 
-http.createServer((req, res) => {
+http.createServer(async (req, res) => {
 
     if (req.url == "/" && req.method == "GET") {
         console.log(req.Url)
@@ -25,7 +26,7 @@ http.createServer((req, res) => {
         req.on("data", (chunk) => {
             body = chunk.toString();
         });
-console.log("en candidato")
+        console.log("en candidato")
         req.on("end", async () => {
             const candidato = JSON.parse(body);
 
@@ -37,17 +38,97 @@ console.log("en candidato")
                 res.statusCode = 500;
                 res.end("ocurrio un error" + e);
             }
-
-
-
         })
+    }
 
 
+    if (req.url == "/candidatos" && req.method == "GET") {
+        try {
+            const candidatos = await getCandidatos();
+            res.end(JSON.stringify(candidatos));
+        } catch (e) {
+            res.statusCode = 500;
+            res.end("ocurrio un error en el servidor" + e);
+        }
     }
 
 
 
 
+    if (req.url == "/candidato" && req.method == "PUT") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body = chunk.toString();
+        });
+        console.log("en candidato")
+        req.on("end", async () => {
+            const candidato = JSON.parse(body);
+
+            try {
+                const result = await editarCandidato(candidato);
+                res.statusCode = 200;
+                res.end(JSON.stringify(result));
+            } catch (e) {
+                res.statusCode = 500;
+                res.end("ocurrio un error" + e);
+            }
+        })
+    }
+
+
+
+
+
+    if (req.url.startsWith("/candidato?id") && req.method == "DELETE") {
+        try {
+            console.log("en eliminar");
+            let { id } = url.parse(req.url, true).query;
+            await eliminarCandidato(id);
+            res.end("candidato eliminado");
+        } catch (e) {
+            res.statusCode = 500;
+            res.end("ocurrio un error en el servidor" + e);
+        }
+    }
+
+
+
+
+
+
+    if (req.url == "/votos" && req.method == "POST") {
+        let body = "";
+        req.on("data", (chunk) => {
+            body += chunk.toString();
+        });
+        console.log("en votos")
+        req.on("end", async () => {
+            try {
+            const voto = JSON.parse(body);
+                const result = await registroVotos(voto);
+                res.statusCode = 201;
+                res.end(JSON.stringify(result));
+            } catch (e) {
+                res.statusCode = 500;
+                res.end("ocurrio un error en el servidor" + e);
+            }
+        })
+    }
+
+
+
+
+
+    if (req.url == "/historial" && req.method == "GET") {
+        try {
+            const historial = await getHistorial();
+            console.log(historial);
+            res.end(JSON.stringify(historial));
+        } catch (e) {
+            res.statusCode = 500;
+            res.end("ocurrio un error en el servidor" + e);
+        }
+    }
 
 
 }).listen(3000, console.log("SERVER ON"))
